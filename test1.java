54ea2739d870795f63fac84b88e8da8dbd052ad4
HyunWoo <mogs223@gmail.com>
2025-03-24 22:30:02 +0900

    인삿말 추가

--- test1.java
+++ test1.java
@@ -5,6 +5,10 @@ public class test1 {
         System.out.println("test파일의 정보를 가리기");
 
         System.out.println("test의 2번 정보 출력");
-
+        System.out.println("app의 2번 정보 출력");
+        System.out.println("app의 2번 정보 출력");
+        System.out.println("app의 2번 정보 출력");
+        System.out.println("hi");
+        System.out.println("hello");
     }
 }
